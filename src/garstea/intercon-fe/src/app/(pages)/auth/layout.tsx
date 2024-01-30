a2e6de90104388c5a3ab5406c0d440288ab91f34
saneag <garstea2002@mail.ru>
2024-01-30 21:05:48 +0200

    feat: added home link on logo image

--- src/garstea/intercon-fe/src/app/(pages)/auth/layout.tsx
+++ src/garstea/intercon-fe/src/app/(pages)/auth/layout.tsx
@@ -1,20 +1,23 @@
 import { ReactNode } from 'react';
 
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className='flex-col-center min-h-screen space-y-4 bg-gray-100'>
-      <Image
-        src='/assets/images/intercon-logo-no-text.png'
-        alt=''
-        width='0'
-        height='0'
-        sizes='100vw'
-        placeholder='blur'
-        blurDataURL='/assets/images/intercon-logo-no-text.png'
-        className='h-auto w-20'
-      />
+      <Link href='/reviews' replace={true}>
+        <Image
+          src='/assets/images/intercon-logo-no-text.png'
+          alt=''
+          width='0'
+          height='0'
+          sizes='100vw'
+          placeholder='blur'
+          blurDataURL='/assets/images/intercon-logo-no-text.png'
+          className='h-auto w-20 cursor-pointer'
+        />
+      </Link>
       {children}
     </div>
   );
