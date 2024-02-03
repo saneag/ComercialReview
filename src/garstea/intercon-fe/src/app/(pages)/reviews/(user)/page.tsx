eb952b5620aa93055507b3fda1a6b82858ad171e
saneag <garstea2002@mail.ru>
2024-02-03 13:14:50 +0200

    feat: added initial reviews filters and list on reviews page

--- src/garstea/intercon-fe/src/app/(pages)/reviews/(user)/page.tsx
+++ src/garstea/intercon-fe/src/app/(pages)/reviews/(user)/page.tsx
@@ -1,3 +1,15 @@
+import ReviewsFilters from '@/app/components/reviews/reviewsFilters';
+import ReviewsList from '@/app/components/reviews/reviewsList';
+
 export default function ReviewsPage() {
-  return <div>Reviews Page</div>;
+  return (
+    <div className='flex gap-10'>
+      <div className='w-3/12'>
+        <ReviewsFilters />
+      </div>
+      <div className='w-full flex-1'>
+        <ReviewsList />
+      </div>
+    </div>
+  );
 }
---
+++ src/garstea/intercon-fe/src/app/components/reviews/reviewsFilters/index.tsx
@@ -0,0 +1,3 @@
+export default function ReviewsFilters() {
+  return <div>Reviews Filters</div>;
+}
