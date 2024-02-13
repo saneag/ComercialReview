4ff1716e64ad4dbb5def4371f63ccc3734f2154d
saneag <garstea2002@mail.ru>
2024-02-13 19:30:38 +0200

    fix: added link display by role and auth status

--- src/garstea/intercon-fe/src/app/components/auth/components/notAuthComponent.tsx
+++ src/garstea/intercon-fe/src/app/components/auth/components/notAuthComponent.tsx
@@ -17,7 +17,7 @@ export default function NotAuthComponent() {
     return () => {
       clearTimeout(timeoutId);
     };
-  }, []);
+  }, [router]);
 
   return (
     <div className='fixed bottom-0 left-0 right-0 top-0 z-50'>
--- src/garstea/intercon-fe/src/app/components/header/links.tsx
+++ src/garstea/intercon-fe/src/app/components/header/links.tsx
@@ -1,8 +1,13 @@
 import Link from 'next/link';
 
 import { adminLinks, privateLinks, publicLinks } from '@/app/constants/routes';
+import { useAppSelector } from '@/app/redux/store';
+import { UserRoleEnum } from '@/app/types/enums/UserRoleEnum';
 
 export default function Links() {
+  const isAuth = useAppSelector((state) => state.user.isAuth);
+  const role = useAppSelector((state) => state.user.role);
+
   return (
     <div className='flex gap-2'>
       <div className='flex gap-2'>
@@ -12,20 +17,25 @@ export default function Links() {
           </Link>
         ))}
       </div>
-      <div className='flex gap-2'>
-        {privateLinks.map((link) => (
-          <Link key={link.label} href={link.path}>
-            <span>{link.label}</span>
-          </Link>
+      {isAuth && (
+        <div className='flex gap-2'>
+          {privateLinks.map((link) => (
+            <Link key={link.label} href={link.path}>
+              <span>{link.label}</span>
+            </Link>
+          ))}
+        </div>
+      )}
+      {role === UserRoleEnum.ADMIN ||
+        (role === UserRoleEnum.SUPER_ADMIN && (
+          <div className='flex gap-2'>
+            {adminLinks.map((link) => (
+              <Link key={link.label} href={link.path}>
+                <span>{link.label}</span>
+              </Link>
+            ))}
+          </div>
         ))}
-      </div>
-      <div className='flex gap-2'>
-        {adminLinks.map((link) => (
-          <Link key={link.label} href={link.path}>
-            <span>{link.label}</span>
-          </Link>
-        ))}
-      </div>
     </div>
   );
 }
