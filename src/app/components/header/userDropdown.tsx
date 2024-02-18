8b0f13c7d93e4e17a64418bd62472602f1a62051
saneag <garstea2002@mail.ru>
2024-02-18 19:10:35 +0200

    feat: added user avatar from shadcn

--- src/app/components/header/userDropdown.tsx
+++ src/app/components/header/userDropdown.tsx
@@ -4,11 +4,20 @@ import { UserRound } from 'lucide-react';
 import Link from 'next/link';
 
 import Dropdown from '@/app/components/dropdown';
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from '@/app/components/ui/avatar';
 import { Button } from '@/app/components/ui/button';
+import { useAppSelector } from '@/app/redux/store';
 import { DropdownContentFields } from '@/app/types/dropdown/DropdownContentFields';
+import { UserRoleEnum } from '@/app/types/enums/UserRoleEnum';
 
 export default function UserDropdown() {
-  const isAuth = false;
+  const isAuth = useAppSelector((state) => state.user.isAuth);
+  const user = useAppSelector((state) => state.user.user);
+  const userRole = useAppSelector((state) => state.user.role);
 
   const [additionalContent, setAdditionalContent] = useState<
     DropdownContentFields[]
@@ -59,8 +68,20 @@ export default function UserDropdown() {
     <div>
       <Dropdown
         triggerText={
-          <div className='cursor-pointer rounded-full bg-gray-400 p-2'>
-            {<UserRound />}
+          <div className='cursor-pointer rounded-full p-0.5 nm-flat-gray-400'>
+            <Avatar>
+              <AvatarImage></AvatarImage>
+              <AvatarFallback className='nm-convex-gray-200'>
+                {user && userRole !== UserRoleEnum.GUEST ? (
+                  <span className='text-capitalize'>
+                    {user.lastName[0]}
+                    {user.firstName[0]}
+                  </span>
+                ) : (
+                  <UserRound />
+                )}
+              </AvatarFallback>
+            </Avatar>
           </div>
         }
         contentLabel='User menu'
