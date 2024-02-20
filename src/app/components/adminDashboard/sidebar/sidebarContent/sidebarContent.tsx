b1ab3adeea2c21ce5e78633e3918268818905515
saneag <garstea2002@mail.ru>
2024-02-20 19:08:58 +0200

    styles: changed styles for admin dashboard sidebar

--- src/app/components/adminDashboard/sidebar/sidebarContent/sidebarContent.tsx
+++ src/app/components/adminDashboard/sidebar/sidebarContent/sidebarContent.tsx
@@ -27,11 +27,11 @@ export default function SidebarContent({
       <div
         className={`h-full flex-col lg:flex ${
           isSidebarOpen
-            ? 'fixed left-10 top-0 z-10 flex bg-white px-5 py-4 animate-in slide-in-from-left-10'
+            ? 'fixed left-10 top-0 z-10 flex w-1/4 bg-white px-5 py-4 animate-in slide-in-from-left-10'
             : 'hidden'
         }`}
       >
-        <div className='space-y-3'>
+        <div className='space-y-5'>
           <HomeLogoLink className='w-10' replace={false}>
             <span className='text-xl'>Intercon</span>
           </HomeLogoLink>
