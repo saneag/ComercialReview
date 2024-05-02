93ff7c549b82640b6ccab8b077159f97e6a7d0b1
saneag <garstea2002@mail.ru>
2024-05-02 22:37:49 +0300

    fix: user get on first load page

--- src/app/providers/AppWrapper.tsx
+++ src/app/providers/AppWrapper.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ReactNode, useEffect } from 'react';
+import { ReactNode, useCallback, useEffect } from 'react';
 
 import { jwtDecode } from 'jwt-decode';
 
@@ -8,6 +8,7 @@ import { Toaster } from '@/app/components/ui/toaster';
 import {
   getUserAccessToken,
   getUserRefreshToken,
+  resetUserOnLogout,
   setUser,
   setUserRole,
 } from '@/app/redux/features/slices/userSlice';
@@ -17,7 +18,7 @@ import { UserJwtClaimsEnum } from '@/app/types/enums/UserJwtClaimsEnum';
 
 export default function AppWrapper({ children }: { children: ReactNode }) {
   const dispatch = useAppDispatch();
-  const [getUser, { data: user }] = useLazyGetUserQuery();
+  const [getUser, { data: user, isError }] = useLazyGetUserQuery();
   const accessToken = useAppSelector((state) => state.user.accessToken);
 
   useEffect(() => {
@@ -25,15 +26,23 @@ export default function AppWrapper({ children }: { children: ReactNode }) {
     dispatch(getUserRefreshToken());
   }, [dispatch]);
 
-  useEffect(() => {
-    if (accessToken) {
-      const decoded: any = jwtDecode(accessToken);
-      const userId = decoded[UserJwtClaimsEnum.UserId];
-      const role = decoded[UserJwtClaimsEnum.Role];
-      dispatch(setUserRole(Number(role)));
-      getUser(userId);
+  const getUserFromToken = useCallback(() => {
+    try {
+      if (accessToken) {
+        const decoded: any = jwtDecode(accessToken);
+        const userId = decoded[UserJwtClaimsEnum.UserId];
+        const role = decoded[UserJwtClaimsEnum.Role];
+        dispatch(setUserRole(Number(role)));
+        getUser(userId);
+      }
+    } catch (error) {
+      dispatch(resetUserOnLogout());
     }
   }, [accessToken, dispatch, getUser]);
+
+  useEffect(() => {
+    getUserFromToken();
+  }, [getUserFromToken]);
 
   useEffect(() => {
     if (user) {
@@ -45,6 +54,12 @@ export default function AppWrapper({ children }: { children: ReactNode }) {
       );
     }
   }, [user, dispatch]);
+
+  useEffect(() => {
+    if (isError) {
+      dispatch(resetUserOnLogout());
+    }
+  }, [dispatch, isError]);
 
   return (
     <>
